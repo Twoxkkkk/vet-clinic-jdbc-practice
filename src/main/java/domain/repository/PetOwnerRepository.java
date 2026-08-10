7ee17a1c2fcdd914be0a554ce5b77dfc48495e6c
Andrei Zheltov <sheltov30@gmail.com>
2026-08-10 19:59:12 +0300

    added: more business logic methods into the PetOwnerRepo

--- src/main/java/domain/repository/PetOwnerRepository.java
+++ src/main/java/domain/repository/PetOwnerRepository.java
@@ -1,7 +1,10 @@
 package domain.repository;
 
+import domain.pet_owner.Pet;
 import domain.pet_owner.PetOwner;
+import domain.shared.Email;
 import domain.shared.Id;
+import domain.shared.Phone;
 
 import java.util.Optional;
 
@@ -11,4 +14,8 @@ public interface PetOwnerRepository {
     void delete(Id<PetOwner> petOwnerId);
 
     Optional<PetOwner> findById(Id<PetOwner> petOwnerId);
+    Optional<PetOwner> findByPhoneNumber(Phone number);
+    Optional<PetOwner> findByEmail(Email email);
+
+    Optional<PetOwner> findByPetId(Id<Pet> petId);
 }
