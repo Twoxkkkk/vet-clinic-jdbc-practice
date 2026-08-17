18b358c973a80f1fce298c167e156197468eaaf7
Andrei Zheltov <sheltov30@gmail.com>
2026-08-17 23:34:27 +0300

    fixed: removed the extended method signature in PetOwnerRepositry interface

--- src/main/java/domain/repository/PetOwnerRepository.java
+++ src/main/java/domain/repository/PetOwnerRepository.java
@@ -2,14 +2,12 @@ package domain.repository;
 
 import domain.pet_owner.PetOwner;
 import domain.shared.Email;
-import domain.shared.Id;
 import domain.shared.Phone;
 
 import java.util.Optional;
 
 public interface PetOwnerRepository extends Repository<PetOwner> {
 
-    Optional<PetOwner> findById(Id<PetOwner> petOwnerId);
     Optional<PetOwner> findByPhoneNumber(Phone number);
     Optional<PetOwner> findByEmail(Email email);
 
